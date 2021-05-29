fb76f24c2c351ce30d3846b0d9b58674dc903ae7
shusingh <ksingh.shubh@gmail.com>
2021-05-29 13:34:48 -0400

    new proj

---
+++ controlled-forms/README.md
@@ -0,0 +1,9 @@
+## Birthday Remider React App
+
+View Live:
+
+This mini-project demonstrates the use and working of forms in React.js
+
+### Screenshot
+
+![](screenshot.png)
---
+++ controlled-forms/build/precache-manifest.3e872bcceac7be0ccf63ebbf11d0a1ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2ebcfccd17d1d479d67554a5d84e384",
+    "url": "/index.html"
+  },
+  {
+    "revision": "67df5a73e1959384f4ec",
+    "url": "/static/css/main.3c469b00.chunk.css"
+  },
+  {
+    "revision": "1a9702781ee7623e7b3f",
+    "url": "/static/js/2.104f0a7f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.104f0a7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67df5a73e1959384f4ec",
+    "url": "/static/js/main.27a76410.chunk.js"
+  },
+  {
+    "revision": "1877e69cc92e77fffa19",
+    "url": "/static/js/runtime-main.147a4b9c.js"
+  }
+]);
